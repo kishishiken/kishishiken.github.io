0c33aca218a694ae85913421d337ec6883392469
kishishiken <kikoshiken@outlook.com>
2026-09-23 18:06:08 +0800

    gu

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ export const themeConfig: ThemeConfig = {
     // site description
     description: 'A normal blog.',
     // use i18n title/subtitle/description from src/i18n/ui.ts instead of static ones above
-    i18nTitle: true, // true | false
+    i18nTitle: false, // true | false
     // author name
     author: 'kishishiken',
     // site url
