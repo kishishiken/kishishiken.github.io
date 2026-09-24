90b523813fef1200df4aef90902c60ac4e562420
kishishiken <kikoshiken@outlook.com>
2026-09-24 16:47:23 +0800

    Update blog.

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,9 +10,9 @@ interface Translation {
 
 export const ui: Record<string, Translation> = {
   'zh-tw': {
-    title: '重新編排',
-    subtitle: '再現版式之美',
-    description: 'Retypeset是一款基於Astro框架的靜態部落格主題，中文名為重新編排。本主題以活版印字為設計靈感，通過建立全新的視覺規範，對所有頁面進行重新編排，打造紙質書頁般的閱讀體驗，再現版式之美。所見皆為細節，方寸盡顯優雅。',
+    title: 'kishishiken',
+    subtitle: 'A normal blog.',
+    description: '',
     posts: '文章',
     bangumi: '番組',
     about: '關於',
